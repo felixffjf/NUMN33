beb9846199da391cba0f4b9997492ac9a77cfed3
Felix Filson <felixfilson@Felixs-MacBook.local>
2026-02-09 03:43:15 +0100

    Refined tasks 4-9

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -5,13 +5,13 @@ import matplotlib.tri as mtri
 #Tasks 5-7
 
 from dune.grid import structuredGrid, cartesianDomain
+domain = cartesianDomain([-0.5, -0.5], [2, 1], [10, 20])
 rect_gridView = structuredGrid([-0.5, -0.5], [2, 1], [10, 20])
-#rect_gridView.plot()
+rect_gridView.plot()
 
 from dune.alugrid import aluConformGrid as leafGridView
-domain = cartesianDomain([-0.5, -0.5], [2, 1], [10, 20])
 tri_gridView = leafGridView(domain)
-#tri_gridView.plot()
+tri_gridView.plot()
 
 
 
@@ -20,8 +20,8 @@ import pygmsh
 
 #Making triangular grid with hole
 with pygmsh.occ.Geometry() as geom:
-    geom.characteristic_length_min = 0.1
-    geom.characteristic_length_max = 0.1
+    geom.characteristic_length_min = 0.05
+    geom.characteristic_length_max = 0.05
     
     grid = geom.add_rectangle([-0.5, -0.5, 0], 2.5, 1.5)
     hole = geom.add_disk([1, 0.5, 0.0], 0.1)
@@ -36,67 +36,34 @@ dune_domain = {
 }
 
 gridView2D = leafGridView(dune_domain)
-
-
-
-
-import meshio
-
-#meshio.write("mesh.vtk", mesh)
-
-##########
-
-
-#Har du ett bättre sätt att plotta?
-
-
-##########
-'''
-# --- Extract triangle connectivity ---
-points = mesh.points[:, :2]          # x,y only
-triangles = mesh.cells_dict["triangle"]
-
-# --- Tell matplotlib how triangles connect ---
-triang = mtri.Triangulation(points[:, 0], points[:, 1], triangles)
-
-# --- Plot ---
-plt.triplot(triang)
-plt.gca().set_aspect("equal")
-plt.show()
-'''
-
+gridView2D.plot()
 
 
 grid_points = mesh.points[:, :2]
 grid_triangles = mesh.cells_dict["triangle"]
 
-h_max = 0.0
 
-for k in range(len(grid_triangles)):
-    t = grid_triangles[k]          
+h_area = 0.0
+for t in grid_triangles:
+    p0, p1, p2 = grid_points[t]
+    area = 0.5 * abs(np.cross(p1 - p0, p2 - p0))  
+    h_area = max(h_area, area**(1/2))
 
-    p0 = grid_points[t[0]]
-    p1 = grid_points[t[1]]
-    p2 = grid_points[t[2]]
-
-    h_max = max(h_max, np.linalg.norm(p1 - p0))
-    h_max = max(h_max, np.linalg.norm(p2 - p1))
-    h_max = max(h_max, np.linalg.norm(p0 - p2))
-
-print(h_max)
-
-
+print(h_area)
 from dune.fem.utility import gridWidth
 h_dune = gridWidth(gridView2D)
 print(h_dune)
 
 
-
-################
-#Dessa stämmer ej överens?
-################
-
-
-
 #Task 10-11
+
+
+
+
+#Task 12-14
+
+def uh(e,xhat,u):
+    p0, p1, p2 = e[0], e[1], e[2]
+    x0hat, x1hat = xhat[0], xhat[1]
+    return u(p0)*(1-x0hat-x1hat) + u(p1)*x0hat + u(p2)*x1hat
 
